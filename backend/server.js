3c97e55f4a1e04fd223d9c6e46588991145b6dbb
aashikmohammedt <aashikmohammedt7@gmail.com>
2026-06-10 23:49:59 +0530

    Debug CORS origins

--- backend/server.js
+++ backend/server.js
@@ -289,6 +289,8 @@ const allowedOrigins = [
   process.env.FRONTEND_URL,
 ].filter(Boolean);
 
+console.log("FRONTEND_URL =", process.env.FRONTEND_URL);
+console.log("allowedOrigins =", allowedOrigins);
 app.use(
   cors({
     origin(origin, callback) {
